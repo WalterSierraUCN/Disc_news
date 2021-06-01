36e1dd5cd12b587ed98bc21fabad41bfe5bbf5c4
Walter Sierra Vega <walter.sierra@alumnos.ucn.cl>
2021-06-01 12:57:36 -0400

    style(Theme and toolbar update): Add theme and toolbar, add colors, add lime.xml, add night version

--- app/src/main/java/clucn/disc/dsm/wsierra/MainActivity.java
+++ app/src/main/java/clucn/disc/dsm/wsierra/MainActivity.java
@@ -3,12 +3,49 @@ package clucn.disc.dsm.wsierra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import clucn.disc.dsm.wsierra.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The Logger
+     */
+
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+    /**
+     *
+     */
+    private ActivityMainBinding binding;
+
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Inflate the xml
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
+
+        // Set toolbar
+        this.setSupportActionBar(this.binding.toolbar);
     }
+
+    /**
+     *
+     */
+    @Override
+    protected void onStart(){
+        super.onStart();
+        log.debug("Onstart !!!");
+    }
+
+
 }
