d501f658601418ab8880cbafc4bbf56c2b732ef6
Walter Sierra Vega <walter.sierra@alumnos.ucn.cl>
2021-07-21 23:58:38 -0400

    feat(New classes added): Added Article class and source class

---
+++ app/src/main/java/clucn/disc/dsm/wsierra/model/newsapi/Article.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021. Walter Sierra Vega
+ */
+
+package clucn.disc.dsm.wsierra.model.newsapi;
+
+public class Article {
+
+    public Source source;
+    public String author;
+    public String title;
+    public String description;
+    public String url;
+    public String urlToImage;
+    public String publishedAt;
+    public String content;
+
+}
---
+++ app/src/main/java/clucn/disc/dsm/wsierra/model/newsapi/Source.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021. Walter Sierra Vega
+ */
+
+package clucn.disc.dsm.wsierra.model.newsapi;
+
+
+public class Source {
+
+    public String id;
+    public String name;
+
+}
